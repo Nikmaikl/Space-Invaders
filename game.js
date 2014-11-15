a0e16fecbdab36c8c33a35f26acdf27b831eb919
Nikmaikl <snownikmaikl@gmail.com>
2014-11-15 20:49:06 +0300

    Added player

--- game.js
+++ game.js
@@ -2,9 +2,13 @@
 	var Game = function(canvasId) {
 		var canvas = document.getElementById(canvasId);
 		var screen = canvas.getContext("2d");
+		var gameSize = {
+			width: canvas.width,
+			height: canvas.height
+		}
 		var self = this;
 
-		this.entities = [new Player()];
+		this.entities = [new Player(gameSize)];
 
 		var tick = function() {
 			self.update();
@@ -16,26 +20,37 @@
 
 	Game.prototype = {
 		update: function() {
-
+			for(var i = 0; i < this.entities.length; i++) {
+				this.entities[i].update();
+			}
 		},
 
 		render: function(screen) {
 			for(var i = 0; i < this.entities.length; i++) {
-				drawRect(screen);
+				drawRect(screen, this.entities[i]);
 			}
 		}
 	}
 
-	Player = function() {
-
+	Player = function(gameSize) {
+		this.size = {
+			width: 32,
+			height: 32
+		}
+		this.position = {
+			x: gameSize.width/2-this.size.width/2,
+			y: gameSize.height/2
+		}
 	}
 
-	Player.prototype = function() {
+	Player.prototype = {
+		update: function() {
 
+		},
 	}
 
-	var drawRect = function(screen) {
-		screen.fillRect(10, 10, 10, 10);
+	var drawRect = function(screen, entity) {
+		screen.fillRect(entity.position.x, entity.position.y, entity.size.width, entity.size.height);
 	}
 
 	window.onload = function() {
